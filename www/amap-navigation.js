45cef1ab2395796a8ccfd99d8e932e8b2a9975aa
JoyoDuan <JoyoDuan@163.com>
2019-09-24 17:04:25 +0800

    Update amap-navigation.js
    asdf

--- www/amap-navigation.js
+++ www/amap-navigation.js
@@ -1,10 +1,10 @@
 var cordova = require('cordova');
 
-function AMapNavigation() {
-}
+// 这里的命令AMapNavigationPlugin与plugin.xml无关，用abc都可以，module.exports用于require('')
+function AMapNavigationPlugin() {}
 
-AMapNavigation.prototype.navigation = function (startPoint, endPoint, NavType, successCallback, errorCallback) {
+AMapNavigationPlugin.prototype.navigation = function (startPoint, endPoint, NavType, successCallback, errorCallback) {
   cordova.exec(successCallback, errorCallback, "AMapNavigationPlugin", 'navigation', [startPoint.lng, startPoint.lat, endPoint.lng, endPoint.lat, NavType.toString()]);
 };
 
-module.exports = new AMapNavigation();
+module.exports = new AMapNavigationPlugin();
